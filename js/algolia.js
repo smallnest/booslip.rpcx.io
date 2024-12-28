d9e5af99f81c9707f316eb20280f466548fc1d16
smallnest <smallnest@gmail.com>
2024-12-28 14:10:07 +0800

    add more

--- js/algolia.js
+++ js/algolia.js
@@ -1,6 +1,6 @@
 (() => {
   // ns-hugo-params:<stdin>
-  var baseURL = "https://booslip.rpcx.io/";
+  var baseURL = "http://localhost:1313/";
   var params = { blogroll: [{ name: "\u9E1F\u7A9D", url: "https://colobu.com/" }], defaultcover: "https://booslip.rpcx.io/covers/bamboo.jpg", email: "smallnest@gmail.com", showcopyright: false, showmore: true, showrss: true, socialmedia: [{ name: "Github", url: "https://github.com/smallnest" }], utterances: true };
 
   // <stdin>
